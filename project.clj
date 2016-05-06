c365aaabbf788f1e1ae8345d5470fc38514522ac
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-05-06 14:31:25 +0300

    Add Rum as a dependency

--- project.clj
+++ project.clj
@@ -30,7 +30,9 @@
                  [conman "0.5.1"]
                  [org.postgresql/postgresql "9.4-1206-jdbc4"]
                  [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
-                 [luminus-immutant "0.2.0"]]
+                 [luminus-immutant "0.2.0"]
+
+                 [rum "0.8.3"]]
 
   :min-lein-version "2.0.0"
 
